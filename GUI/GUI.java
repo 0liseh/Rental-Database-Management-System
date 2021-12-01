0109fa5da5e2d6591fde42d0d60f42c608523618
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2021-12-01 11:00:00 -0700

    Controller, Data, GUI folders made

---
+++ GUI/GUI.java
@@ -0,0 +1,10 @@
+//Other GUIs inherit this class
+import javax.swing.*;                           //package for GUIs
+
+class GuiTest extends javax.swing.JFrame{
+
+
+  public static void main(String[] args){
+
+  }
+}
