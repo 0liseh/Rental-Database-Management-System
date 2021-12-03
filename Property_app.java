ed0c905e017a2f9d78b973d7b831ab8b61721666
colin <colin.christophe@ucalgary.ca>
2021-12-03 17:50:15 +0000

    Property_App out of packages

--- Property_app.java
+++ Property_app.java
@@ -2,14 +2,18 @@
   The first UI the user comes in contact with
   Will call whatever GUI the user needs
 */
+//Property_app.java
+package GUI;
+import Data.*;
+import Controller.*;
 
 class Property_app{
 
   private GUI setupGUI;
   
-  public setupWindows(){
-
-  }
+  /*public setupWindows(){
+    
+  }*/
 
   public static void main(String[] args){
 
