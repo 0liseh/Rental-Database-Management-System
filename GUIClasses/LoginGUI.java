701c2124ccde745499e610afdedd0ebac0e80042
colin <colin.christophe@ucalgary.ca>
2021-12-04 20:29:51 +0000

    Login Page View

--- GUIClasses/LoginGUI.java
+++ GUIClasses/LoginGUI.java
@@ -10,46 +10,56 @@ public class LoginGUI extends GUI{
   //private Logout logoutObj;
   private LoginController logoutObj;
   private Font normalFont = new Font("Courier", Font.PLAIN, 15);
-  private JPanel loginPanel = new JPanel();
   private JTextField un = new JTextField();
   private JTextField pw = new JTextField();
 
   public LoginGUI(){
-
-    
-    
     JButton login = new JButton("Login");
     JButton guest = new JButton("Use as Guest");
-    //GridLayout L = new GridLayout(4, 2, 20, 20);
+    
+   
+    
 
     JLabel header = new JLabel("Login to Property Tracker!", JLabel.CENTER);
     header.setForeground(Color.BLACK);
     header.setFont(new Font("Courier", Font.PLAIN, 30));
     header.setBounds(10, 10 , 480, 40);
     panel.add(header);
-    //frame.setContentPane(loginPanel);
     
-    //panel.setLayout(L);
 
-
-    /*JLabel username = new JLabel("Username:");
+    JLabel username = new JLabel("Username:");
     username.setForeground(Color.BLACK);
     username.setFont(normalFont);
-    panel.add(username);*/
-   //panel.add(un);
-    //username.setBounds(30, 50 , 480, 40);
-    
+    username.setBounds(40, 50 , 480, 40);
+    panel.add(username);
 
-    /*JLabel password = new JLabel("Password:");
+    un.setBounds(40 , 90, 420, 30);
+    un.setFont(normalFont);
+    panel.add(un);
+
+    
+    JLabel password = new JLabel("Password:");
     password.setForeground(Color.BLACK);
     password.setFont(normalFont);
     panel.add(password);
-    password.setBounds(10, 50 , 480, 40);
+    password.setBounds(40, 120 , 480, 40);
 
+    pw.setBounds(40, 160, 420, 30);
+    pw.setFont(normalFont);
+    panel.add(pw);
     
-    
+    login.setBounds(40 , 210, 420, 30);
+    login.setBackground(Color.GRAY);
+    login.setForeground(Color.WHITE);
+    login.setFont(normalFont);
     panel.add(login);
-    panel.add(guest);*/
+
+
+    guest.setBounds(40 , 250, 420, 30);
+    guest.setBackground(Color.GRAY);
+    guest.setForeground(Color.WHITE);
+    guest.setFont(normalFont);
+    panel.add(guest);
   }
 
   public void confirmLogin(){
