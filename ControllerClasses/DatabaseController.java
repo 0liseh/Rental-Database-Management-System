cce06ee28dca9d6c150597493a7f2bb90b768acc
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2021-12-08 19:57:45 -0700

    getProperties

--- ControllerClasses/DatabaseController.java
+++ ControllerClasses/DatabaseController.java
@@ -1,4 +1,4 @@
-package ControllerClasses;
+import DataClasses.Property;
 
 import java.sql.*;
 import java.sql.Connection;
@@ -6,9 +6,12 @@ import java.sql.Driver;
 import java.sql.DriverManager; //uses static method getConnection(database URL, username, password)
 import java.sql.SQLException;
 import java.util.*;
+import java.lang.*;
 
 public class DatabaseController{
   
+	private Vector<Property> properties = new Vector<Property>();
+	
 	private static final String url = "jdbc:mysql://localhost:3306/propertymanagement";
 	private static final String username = "root";
 	private static final String password = "JesusistheOGM1!";
@@ -34,9 +37,8 @@ public class DatabaseController{
 	public void getUsers() {
 		try {
 			//Class.forName("com.mysql.cj.jdbc.Driver");
-			//mysql_con = DriverManager.getConnection(url,username,password);
-			stmt=mysql_con.createStatement();  
-			rs=stmt.executeQuery("select * from user");  
+			stmt = mysql_con.createStatement();  
+			rs = stmt.executeQuery("select * from user");  
 			
 			while(rs.next())  
 				System.out.println(rs.getString("username")+"\t\t\t\t\t"+rs.getInt("id")+"\t\t\t"+rs.getInt("password1"));
@@ -50,5 +52,30 @@ public class DatabaseController{
 	public void removeUser() {}
 	public void addProperty() {}
 	public void removeProperty() {}
-	public void getProperties() {}
+	
+	public void getProperties() {
+		try {
+			//Class.forName("com.mysql.cj.jdbc.Driver");
+			stmt = mysql_con.createStatement();  
+			rs = stmt.executeQuery("select * from property");  
+			
+			while(rs.next()) {
+				Property temp;
+				int propID = rs.getInt("propertyId");
+				String propType = rs.getString("propertyType");
+				int numOfBed = rs.getInt("numberOfBed");
+				int numOfBath = rs.getInt("numberOfBath");
+				boolean furn = rs.getBoolean("furnished");
+				String area = rs.getString("area");
+				String status = rs.getString("status1");
+				int llID = rs.getInt("landlordID");
+				
+				temp = new Property(propID, propType, numOfBed, numOfBath, furn, area, status, llID);
+				properties.add(temp);
+			}
+		}
+		catch(Exception e){ 
+			System.out.println(e);
+		}  
+	}
 }
