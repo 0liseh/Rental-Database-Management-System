ea7b1f8036c3addb0f75db05cfd283f533bfa46b
colin <colin.christophe@ucalgary.ca>
2021-12-10 01:13:21 +0000

    g

--- GUIClasses/RegisterGUI.java
+++ GUIClasses/RegisterGUI.java
@@ -20,7 +20,7 @@ public class RegisterGUI extends  GUI{
 
     public RegisterGUI(){
         controlPanel.setLayout(new GridLayout(0 , 1 ,0 , 20));
-        controlPanel.setBorder(BorderFactory.createEmptyBorder(15, 40, 10, 40));
+        //controlPanel.setBorder(BorderFactory.createEmptyBorder(15, 40, 10, 40));
         dbController = new DatabaseController();
         setTextFields();
         setButtons();
